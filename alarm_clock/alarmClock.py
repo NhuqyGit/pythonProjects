f29186f8be5880e8ce45933f3c21a651f0a16795
NhuqyGit <kathonFLAG@gmail.com>
2022-01-08 20:13:15 +0700

    Add new project alarm

---
+++ alarm_clock/alarmClock.py
@@ -0,0 +1,19 @@
+import time
+from playsound import playsound
+from datetime import datetime
+
+t = input("Input time you want the alarm to ring (hour:min)?").split(":")
+AP = input("Am or Pm?")
+
+if AP == "Pm":
+    t[0] = str(int(t[0]) + 12)
+
+while True:
+    if int(t[0]) == datetime.now().hour and int(t[1]) == datetime.now().minute:
+        print("Its time!!!!")
+        playsound("C:/Users/DELL/OneDrive/Máy tính/openCV/alarm_tones.mp3")
+        time.sleep(1)
+        playsound("C:/Users/DELL/OneDrive/Máy tính/openCV/alarm_tones.mp3")
+        time.sleep(1)
+        playsound("C:/Users/DELL/OneDrive/Máy tính/openCV/alarm_tones.mp3")
+        break 
